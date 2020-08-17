14ec7ba1ed80490a20ac3c0e6b11de6ad4677ea2
Michael Wiebusch <m.wiebusch@gsi.de>
2020-08-17 13:30:50 +0200

    refactored send_csv.py

--- send_csv.py
+++ send_csv.py
@@ -1,130 +1,160 @@
 #!/usr/bin/env python3
-import SCPI_socket as sock
-import struct
+
+import M8195A as awg
+from M8195A import spice_float as float
+
 from time import sleep
 import numpy as np
 import sys
-
-from scipy import interpolate
-
-def resample(target_x,data_x,data_y,**kwargs):
-  fill_value = float(kwargs.get("fill_value",0.))
-  f = interpolate.interp1d(data_x,data_y,bounds_error=False, fill_value=fill_value)
-  out_x = target_x
-  out_y = f(target_x)
-  return (out_x,out_y)
-
-
+import os
 
 
 def send_csv(**kwargs):
 
-  my_file     = kwargs.get("file",0)
   delimiter   = str(kwargs.get("delimiter",","))
+  
+  my_file     = str(kwargs.get("file",""))
+
   trace       = int(kwargs.get("trace",1))
-  #on_val      = float(kwargs.get("on_val",0.5))
   idle_val    = float(kwargs.get("idle_val",0))
   yscale      = float(kwargs.get("yscale",1))
   xscale      = float(kwargs.get("xscale",1))
-  #width       = float(kwargs.get("width",50e-9))
   delay       = float(kwargs.get("delay",0e-9))
-  #offset      = float(kwargs.get("offset",0e-9))
   sample_rate = int(float(kwargs.get("sample_rate",65e9)))
   invert      = int(kwargs.get("invert",0))
   ip          = str(kwargs.get("ip","192.168.0.203"))
+  
+  period      = float(kwargs.get("period",0))
 
-  if ((sample_rate < 53.76e9) or (sample_rate > 65e9)):
-    raise NameError('sample rate must be >=53.76e9 and <= 65.0e9')
+
+  ycol       = str(kwargs.get("ycol","1"))
+  ch1col     = str(kwargs.get("ch1col",""))
+  ch2col     = str(kwargs.get("ch2col",""))
+  ch3col     = str(kwargs.get("ch3col",""))
+  ch4col     = str(kwargs.get("ch4col",""))
+
+  watch_changes  = int(kwargs.get("watch_changes",0))
+  
+  
+  
+  
+  
+  
+
+  multichan_dic = {}
+
+  if ((ycol != "") and (trace <= 4) and (trace >=1)):
+    multichan_dic[trace] = int(ycol)
+
+  if (ch1col != ""):
+    multichan_dic[1] = int(ch1col)
+
+  if (ch2col != ""):
+    multichan_dic[2] = int(ch2col)
+
+  if (ch3col != ""):
+    multichan_dic[3] = int(ch3col)
+
+  if (ch4col != ""):
+    multichan_dic[4] = int(ch4col)
+
+  if (len(multichan_dic.keys()) == 0):
+    print("I got no signal= argument. Stop.")
+    exit()
+
+
 
   
-  if (my_file == 0):
-    raise NameError("no file=<file> argument given")
+  if (my_file == ""):
+    print("no file=<file> argument given")
+    exit()
+
+  if (os.path.exists(my_file) == False):
+    raise NameError("file {} does not exist!".format(my_file))
+    exit()
 
 
-  data = np.loadtxt(my_file, delimiter=delimiter)
-  xdata = data[:,0]*xscale
-  xdata += delay
+  last_mod_date = 0
 
-  width = xdata[-1]
-
-  ydata = data[:,1]*yscale
-
-
-  target_x = np.arange(0,width,1./sample_rate)
-  target_x , target_y = resample(target_x,xdata,ydata,fill_value=idle_val)
-
-  if( np.max(np.abs(target_y)) > 0.5):
-    print("####################################")
-    print("## WARNING: Waveform will clip!!! ##")
-    print("####################################")
-
-  # clip to allowed value range
-  target_y[target_y > 0.5] = 0.5
-  target_y[target_y < -0.5] = -0.5
-
-
-  #volt        = float(kwargs.get("volt",0.5))
-  offset = 0
-  volt = np.max(np.abs(target_y))
-  idle_val = idle_val/volt
-  target_y = target_y*127./volt
-  volt = volt*2
-
-  if(invert):
-    idle_val = -idle_val
-    target_y = -target_y
-
-
-  idle_val_dac = int(idle_val*127)
-
-  #n_delay = int(delay*sample_rate) 
-  n_offset = int(offset*sample_rate) 
-  n = int(len(target_x))
+  loop_cntr = 0
   
-  # sample len must be a multiple of 128
-  sample_len = np.max([int((n)/128+1)*128,128]) # multiples of 128
-  #print("sample len :{:d}".format(sample_len))
-  
-  #dataList = [-100 for i in range(sample_len)]
-  
-  dataList = idle_val_dac*np.ones(sample_len)
-  
-  dataList[0:n] = target_y
-  dataList = dataList.astype(np.int).tolist()
-  
-  dataString = ",".join(map(str,dataList))
-  cmdString = ":TRAC{:d}:DATA 1,{:d},{}".format(trace,n_offset,dataString)
-  
-  # Open socket, create waveform, send data, read back and close socket
-  session = sock.SCPI_sock_connect(ip)
 
-  print(sock.SCPI_sock_query(session,"*idn?"))
+  while(1):
+    
+    # get .raw file modification date
+    mod_date = os.path.getmtime(my_file)
 
-  sock.SCPI_sock_send(session,":INIT:IMM")
-  print("sample rate (Hz):")
-  sock.SCPI_sock_send(session,":SOUR:FREQ:RAST {:d}".format(int(sample_rate)))
-  print(sock.SCPI_sock_query(session,":SOUR:FREQ:RAST?"))
-  
-  sock.SCPI_sock_send(session,":ABOR")
-  
-  print(sock.SCPI_sock_query(session,":TRAC{:d}:CAT?".format(trace)))
-  sock.SCPI_sock_send(session,":TRAC{:d}:DEL:ALL".format(trace))
-  sock.SCPI_sock_send(session,":TRAC{:d}:DEF 1,262144,{:d}".format(trace,idle_val_dac))
-  
-  #send data
-  print("sending data ...")
-  sock.SCPI_sock_send(session,cmdString)
-  #print(sock.SCPI_sock_query(session,":TRAC1:DATA? 1,0,512"))
+    if ( mod_date != last_mod_date):
+      if (watch_changes):
+        print(" ")
+        print("csv input file has changed!")
 
-  print("set output voltage ...")
-  sock.SCPI_sock_send(session,":VOLT{:d} {:3.3f}".format(trace,volt))
+      last_mod_date = mod_date
 
-  print("Output {:d} on ...".format(trace))
-  sock.SCPI_sock_send(session,":OUTP{:d} ON".format(trace))
-  print("run!")
-  sock.SCPI_sock_send(session,":INIT:IMM")
-  print("close socket")
-  sock.SCPI_sock_close(session)
+      session = awg.open_session(ip)
+
+      awg.set_sample_rate(sample_rate)
+
+      print("read csv file \"{}\"".format(my_file))
+      try:
+        data = np.loadtxt(my_file, delimiter=delimiter)
+      except:
+        raise NameError("sth went wrong while reading csv file \"{}\"".format(my_file))
+      finally:
+        print("success!")
+
+        
+      for trace in multichan_dic.keys():
+      
+        ch_data_col = multichan_dic[trace]
+        print("ch{:d} data is in csv col {:d}".format(trace,ch_data_col))
+        
+        xdata = data[:,0]*xscale
+        xdata += delay
+      
+        ydata = data[:,ch_data_col]*yscale
+        
+        print("success!")
+        
+        awg.program_trace( xdata, ydata, 
+                           trace       = trace,
+                           idle_val    = idle_val,
+                           xscale      = xscale,
+                           yscale      = yscale,
+                           delay       = delay,
+                           invert      = invert,
+                           sample_rate = sample_rate,
+                           period      = period
+                        )
+
+
+
+      # done with individual trace stuff
+
+      awg.run()
+      awg.close_session()
+
+      if (watch_changes == 0):
+        break
+      else:
+        print ("--------------------------------------------------")
+        print ("watching file {}, will reprogram AWG on change ...".format(my_file)) 
+        print ("press CTRL+C if you want to abort")
+
+    
+    sleep(1) 
+    
+    # display funny scanning animation
+    print(loop_cntr*"_"+"#"+(9-loop_cntr)*"_",end="\r")
+    loop_cntr = (loop_cntr +1)%10
+  
+  
+
+
+
+
+
+
   
   
 
